44adb86d1d0269431aa53fe2bb18ce3519a2481a
Lindsey Shorser <lshorser@teach.cs.toronto.edu>
2020-01-13 22:52:19 -0500

    Updated the code for FruitStand.

--- src/Grape.java
+++ src/Grape.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Grape extends Fruit {
+
+    public String name = "Grape";
+    public boolean isForSale = true;
+    public static String salesCat = "individual produce";
+
+    public Grape() {
+        super("Fruit");
+    }
+
+    public static String getSalesCat() {
+        return salesCat;
+    }
 
 public class Grape {
 }
--- src/Pear.java
+++ src/Pear.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class Pear extends Fruit {
+
+    public String name = "Pear";
+    public boolean isForSale = true;
+    public static String salesCat = "individual produce";
+
+    public Pear() {
+        super("Fruit");
+    }
+
+    public void setIsForSale(boolean status) {
+        this.isForSale = status;
+    }
+
+    public boolean getIsForSale() {
+        return isForSale;
+    }
+
+    public static String getSalesCat() {
+        return salesCat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grape fruitSalad(){
+        return new Grape();
+    }
 
 public class Pear {
 }
